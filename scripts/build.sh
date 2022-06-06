78bf0019c3a11a890a433c7a4af615b00f951427
nisiyong <stephen.ni@outlook.com>
2022-06-06 23:22:08 +0800

    build: set meaningful default values for build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,17 +16,17 @@
 #
 
 # apollo config db info
-apollo_config_db_url='jdbc:mysql://fill-in-the-correct-server:3306/ApolloConfigDB?characterEncoding=utf8'
-apollo_config_db_username='FillInCorrectUser'
-apollo_config_db_password='FillInCorrectPassword'
+apollo_config_db_url='jdbc:mysql://localhost:3306/ApolloConfigDB?characterEncoding=utf8'
+apollo_config_db_username='root'
+apollo_config_db_password=''
 
 # apollo portal db info
-apollo_portal_db_url='jdbc:mysql://fill-in-the-correct-server:3306/ApolloPortalDB?characterEncoding=utf8'
-apollo_portal_db_username='FillInCorrectUser'
-apollo_portal_db_password='FillInCorrectPassword'
+apollo_portal_db_url='jdbc:mysql://localhost:3306/ApolloPortalDB?characterEncoding=utf8'
+apollo_portal_db_username='root'
+apollo_portal_db_password=''
 
 # meta server url, different environments should have different meta server addresses
-dev_meta=http://fill-in-dev-meta-server:8080
+dev_meta=http://localhost:8080
 fat_meta=http://fill-in-fat-meta-server:8080
 uat_meta=http://fill-in-uat-meta-server:8080
 pro_meta=http://fill-in-pro-meta-server:8080
